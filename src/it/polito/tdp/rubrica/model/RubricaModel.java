8221c454cd4dca974c1f6a1fe1e56c2e52f35efa
Fulvio Corno <fulvio.corno@polito.it>
2016-03-14 16:32:53 +0100

    Parte sviluppata in aula il 14/03/2016

---
+++ src/it/polito/tdp/rubrica/model/RubricaModel.java
@@ -0,0 +1,27 @@
+package it.polito.tdp.rubrica.model;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * L'insieme di tutte le {@link Voce} della rubrica,
+ * ed i metodi per poter gestire tale insieme
+ * @author Fulvio
+ *
+ */
+public class RubricaModel {
+
+	private List<Voce> rubrica ;
+	
+	public RubricaModel() {
+		this.rubrica = new ArrayList<Voce>() ;
+	}
+
+	public void addVoce(Voce v) {
+		
+	}
+	
+	public Voce findVoceByNome(String nome) {
+		
+	}
+}
